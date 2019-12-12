7393af9a5d3ad7d8879b73f7cd673c15c8f7c493
kitty <name>
2019-12-12 22:41:27 +0800

    [FIX]修正上帝区块的前置哈希值

--- src/block/block.go
+++ src/block/block.go
@@ -23,7 +23,7 @@ type Block struct {
 
 //NewGenesisBlock 根据配置生成新区块
 func NewGenesisBlock(coinbase *transaction.Tx) *Block {
-	return NewBlock([]*transaction.Tx{coinbase}, "0x0", 0)
+	return NewBlock([]*transaction.Tx{coinbase}, "", 0)
 }
 
 //NewBlock 新增普通区块
